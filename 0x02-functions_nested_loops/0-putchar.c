39eeb3af10bfd76c90fe6b3489fb8683973ae070
root <funsho75@yahoo.com>
2022-04-13 02:27:18 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ *main block
+ *to prin _putcar
+ *and a new line
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h_");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
+	}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifindef _MAIN_N_
+#define _MAIN_N
+
+int _putchar (char c);
+void print_alphabet (void);
+
+endif /*_MAIN_N*/
+
